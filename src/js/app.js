23d6e6971db56b7af4eecc1a689b6ac6d138e909
jasonc-piano <jasonc.piano@gmail.com>
2017-06-24 12:51:07 -0400

    Base app

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,1 @@
-// Your code goes here
-
-var vinElem = document.getElementById('vin');
-gm.info.getVehicleConfiguration(function(data) {
-  vinElem.innerHTML = gm.info.getVIN();
-});
+// Main app JS code
