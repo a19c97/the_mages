c2e6afae1341087e0d30ae55afaf28ed700440a9
AsicChen <a19c97@hotmail.com>
2017-06-24 14:34:42 -0400

    http update

--- src/js/functions.js
+++ src/js/functions.js
@@ -10,3 +10,25 @@ function closeNav() {
 
     document.getElementById("mySidenav").style.width = "0";
 }
+
+
+// Server side code
+var http = require('http');
+
+// Speeding
+
+/* Speeding warning */
+function speedingWarning() {
+
+}
+
+/* Send speeding data to mom */
+function sendSpeedingText() {
+
+}
+
+// Location data
+/* Send location data */
+function sendLocation() {
+
+}
