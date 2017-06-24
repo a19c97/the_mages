40e49a50874041688ec9fb4b887f5a8a8d987948
christinekc <christinekc.cheng@gmail.com>
2017-06-24 15:09:45 -0400

    gohome

--- src/js/functions.js
+++ src/js/functions.js
@@ -10,7 +10,20 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
 
+var dest = {
+  address: '400 Renaissance Center Drive, Detroit, Michigan 48243'
+}
+
 /* Set the destination of the car to home */
 function goHome() {
-	
+	console.log('go home');
+	//gm.nav.setDestination(goHomeSuccess, goHomeFailure, dest, true);
+}
+
+function goHomeSuccess(list) {
+  console.log('destination has been set');
+}
+
+function goHomeFailure(err) {
+  console.log('having trouble setting destination');
 }
