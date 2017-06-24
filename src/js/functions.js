f31260f9bf3378a4f0e06f1fafe159fbd040a2b5
jasonc-piano <jasonc.piano@gmail.com>
2017-06-24 16:38:53 -0400

    Delete sidemenu

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,15 +1,5 @@
 // App JS functions
 
-/* Set the width of the side navigation to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "200px";
-}
-
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
-
 // Server side code
 var request = require('request');
 //request();
