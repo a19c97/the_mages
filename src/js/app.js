2f202ab1658ff961ae17c48c3799b0bed9858001
AsicChen <a19c97@hotmail.com>
2017-06-25 00:01:43 -0400

    update

--- src/js/app.js
+++ src/js/app.js
@@ -7,43 +7,7 @@ var currentUser = {};
 var users = [];
 // location
 var prevTime = 0;
-// Learner mode
-var learnerMode = false;
+
+var userCreated = false;
 
 initializeUI();
-checkLocation();
-checkSpeed();
-
-function checkSpeed(){
-    console.log("Running checkSpeed");
-    var speedLimit = 80;
-    var id = gm.info.watchVehicleData(getSpeedSuccess, ['average_speed']);
-
-    function getSpeedSuccess(data) {
-        if (data.average_speed > (speedLimit + 20)) {
-            speedingWarning(data.average_speed);
-        } else if (data.average_speed < speedLimit) {
-            resetWarning(data.average_speed);
-        }
-    }
-}
-
-function checkLocation(){
-    var id = gm.info.watchPosition(processPosition, true);
-    var thres = 10;
-
-    function processPosition(position){
-        var lat = position.coords.latitude;
-        var lon = position.coords.longitude;
-        console.log("lat: " + lat + "lon: " + lon);
-
-        var currTime = Date.now();
-
-        if(currTime - prevTime > 10000){
-            console.log("Wow we've moved a lot");
-            prevTime = currTime;
-            sendLocation(lon, lat);
-        }
-    }
-
-}
--- src/js/functions.js
+++ src/js/functions.js
@@ -26,6 +26,7 @@ function createUser() {
     var parent = document.getElementById('welcome').children[2];
     parent.insertBefore(newIcon, parent.firstChild);
     changePageFocus('welcome', null);
+    userCreated = true;
 }
 
 // Speeding
@@ -130,4 +131,38 @@ function myMap() {
         zoom: 10
     }
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
+
+function checkSpeed(){
+    console.log("Running checkSpeed");
+    var speedLimit = 80;
+    var id = gm.info.watchVehicleData(getSpeedSuccess, ['average_speed']);
+
+    function getSpeedSuccess(data) {
+        if (data.average_speed > (speedLimit + 20)) {
+            speedingWarning(data.average_speed);
+        } else if (data.average_speed < speedLimit) {
+            resetWarning(data.average_speed);
+        }
+    }
+}
+
+function checkLocation(){
+    var id = gm.info.watchPosition(processPosition, true);
+    var thres = 10;
+
+    function processPosition(position){
+        var lat = position.coords.latitude;
+        var lon = position.coords.longitude;
+        console.log("lat: " + lat + "lon: " + lon);
+
+        var currTime = Date.now();
+
+        if(currTime - prevTime > 10000){
+            console.log("Wow we've moved a lot");
+            prevTime = currTime;
+            sendLocation(lon, lat);
+        }
+    }
+
 }
