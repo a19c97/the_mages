932971fc9f495920d7890f8c72dd7b1e2071342b
AndreyBorisKhesin <andrey.khesin@gmail.com>
2017-06-25 02:28:20 -0400

    doubled request

--- src/js/poi.js
+++ src/js/poi.js
@@ -2,7 +2,7 @@ function getPOI(type, tabName, elmnt, r, g, b) {
 	var xhr = new XMLHttpRequest();
 	var lat;
 	var lon;
-	gm.info.getCurrentPosition(setCoords)
+	gm.info.getCurrentPosition(setCoords, true)
 	
 	openTab(tabName, elmnt, r, g, b);
 	
@@ -21,6 +21,16 @@ function getPOI(type, tabName, elmnt, r, g, b) {
 	
 	xhr.send(null);
 
+	xhr.open('GET', 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + lat + ',' + lon + '&radius=1000&type=' + type + '&key=AIzaSyDBo-j489qE4Ea7UOkJvCMD_SJnlt2xfXs');
+	var r
+	xhr.addEventListener('load', assignResult);
+	
+	function assignResult() {
+		r = JSON.parse(xhr.responseText);
+	}
+	
+	xhr.send(null);
+	
 	var result = new Array(Math.min(r.results.length, 10));
 
 	for (var i = 0; i < result.length; i++) {
