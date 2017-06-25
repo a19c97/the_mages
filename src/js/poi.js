f45cf1402326105f4e5f1f78a24e1a75bb10eb62
AndreyBorisKhesin <andrey.khesin@gmail.com>
2017-06-25 06:24:22 -0400

    got pois working... i think

--- src/js/poi.js
+++ src/js/poi.js
@@ -1,41 +1,13 @@
 function getPOI(type, tabName, elmnt, color) {
-	var xhr = new XMLHttpRequest();
 	var lat;
 	var lon;
-	gm.info.getCurrentPosition(setCoords, true)
+	gm.info.getCurrentPosition(processPosition, true)
 	
+	function processPosition(position){
+		lat = position.coords.latitude;
+		lon = position.coords.longitude;
+	}
+
 	openTab(tabName, elmnt, color);
-	
-	function setCoords(data) {
-		lat = data.coords.latitude;
-		lon = data.coords.longitude;
-	}
-	
-	xhr.open('GET', 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + lat + ',' + lon + '&radius=1000&type=' + type + '&key=AIzaSyDBo-j489qE4Ea7UOkJvCMD_SJnlt2xfXs');
-	var r
-	xhr.addEventListener('load', assignResult);
-	
-	function assignResult() {
-		r = JSON.parse(xhr.responseText);
-	}
-	
-	xhr.send(null);
-
-	xhr.open('GET', 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + lat + ',' + lon + '&radius=1000&type=' + type + '&key=AIzaSyDBo-j489qE4Ea7UOkJvCMD_SJnlt2xfXs');
-	var r
-	xhr.addEventListener('load', assignResult);
-	
-	function assignResult() {
-		r = JSON.parse(xhr.responseText);
-	}
-	
-	xhr.send(null);
-	
-	var result = new Array(Math.min(r.results.length, 10));
-
-	for (var i = 0; i < result.length; i++) {
-		result[i] = r.results[i].name;
-	}
-
-	return result;
+	return init(type, lat, lon);
 }
